ccb903c49aad8bd24b8f4c6072a62cbfeb51b1dd
AmaniDunia.Apps <apps@amanidunia.de>
2024-03-18 03:02:22 +0100

    NavigationBottomBar with 4 Tabs Background is changing Gradient

---
+++ app/src/main/java/de/amanidunia/apps/navigationbartest/MainActivity.kt
@@ -0,0 +1,69 @@
+package de.amanidunia.apps.navigationbartest
+
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.view.inputmethod.InputBinding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import de.amanidunia.apps.navigationbartest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        supportActionBar?.hide()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragement(Home())
+
+        val homeView = findViewById<ConstraintLayout>(R.id.main)
+
+        val drawable : AnimationDrawable = homeView.background as AnimationDrawable
+
+        drawable.setEnterFadeDuration(5500)
+        drawable.setExitFadeDuration(7000)
+        drawable.start()
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.home -> replaceFragement(Home())
+                R.id.drink -> replaceFragement(Drink())
+                R.id.eat -> replaceFragement(Eat())
+                R.id.cart -> replaceFragement(Cart())
+
+                else -> {
+
+                }
+            }
+            true
+
+
+        }
+    }
+
+    private fun replaceFragement(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+
+    }
+}
+
+
